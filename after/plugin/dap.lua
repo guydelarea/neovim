02606ea354e9df007190b667a420185e24b24362
Guy Delarea <guy_delarea_tp@bmc.com>
2023-03-09 18:43:14 +0200

    adding transpaernt bg and fixed dap for python debugging

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,6 +1,16 @@
-local dap = require('dap')
-dap.adapters.python = {
-  type = 'executable';
-  command = '/usr/bin/python3';
-  args = { '-m', 'debugpy.adapter' };
-}
+--local dap = require('dap')
+require('dap-python').setup('/home/guy/bmc/saas_deployment_scripts/venv/bin/python3')
+--dap.adapters.python = {
+--  type = 'executable';
+--  command = '/usr/bin/python3';
+--  args = { '-m', 'debugpy.adapter' };
+--}
+table.insert(require('dap').configurations.python, {
+  type = 'python',
+  request = 'launch',
+  name = 'My custom launch configuration',
+  program = '${file}',
+  -- ... more options, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings
+})
+vim.fn.sign_define('DapBreakpoint',{ text ='🟥', texthl ='', linehl ='', numhl =''})
+vim.fn.sign_define('DapStopped',{ text ='▶️', texthl ='', linehl ='', numhl =''})
