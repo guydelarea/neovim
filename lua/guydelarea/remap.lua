e89f0259061204f20920017977aaebbf531fe641
Guy Delarea <guy_delarea_tp@bmc.com>
2023-03-12 16:12:12 +0200

    Fixing Dap Keybinds -> WSL Support F9|F10|F12

--- lua/guydelarea/remap.lua
+++ lua/guydelarea/remap.lua
@@ -53,7 +53,7 @@ vim.api.nvim_set_keymap("n", "<leader>gr", "<cmd>BufferMovePrevious<CR>", { nore
 vim.keymap.set('n', '<F3>', function() require("dapui").toggle() end)
 vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
 vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
-vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F9>', function() require('dap').step_into() end)
 vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
 vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
 vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
